c7a77ceab47baa9ca6f5bc3705f2db7c7497fb7e
Michael <micjanic20@gmail.com>
2020-07-15 21:02:54 -0500

    serve frontend

--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import App from './client/components/App'
-
-// ReactDOM.render(<App />, document.getElementById('app'))
-
 import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
+import path from 'path'
 
-import Database from './server/utils/database'
-import irrigationRouter from './server/resources/irrigation/irrigation.router'
+const server = express()
 
-const app = express()
+const staticMiddleware = express.static('dist')
 
-// adding Helmet to enhance your API's security
-app.use(helmet())
+server.use(staticMiddleware)
 
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.json())
+server.get('/', function(req, res) {
+    res.sendFile(__dirname + 'dist/index.html')
+})
 
-// enabling CORS for all requests
-app.use(cors())
-
-// adding morgan to log HTTP requests
-app.use(morgan('combined'))
-
-app.use('/api/irrigation', irrigationRouter)
-
-const start = async () => {
-    const port = process.env.PORT || 3001
-    try {
-        await new Database()
-        app.listen(port, () => {
-            console.log(`http://localhost:${port}/api`)
-        })
-    } catch (e) {
-        console.error(e)
-    }
-}
-
-start()
+const port = 3000
+server.listen(port, () => console.log(`listening on ${port}`))
